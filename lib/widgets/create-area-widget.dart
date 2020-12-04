800b44139f3e569762177bbeffc3fe2dcec64649
Tien Viet <vietnt.vn@gmail.com>
2020-12-05 00:47:59 +0700

    update

--- lib/widgets/create-area-widget.dart
+++ lib/widgets/create-area-widget.dart
@@ -4,9 +4,17 @@ import 'package:vietagri/pages/area/create-area.dart';
 Widget createAreaWidget(BuildContext context) {
   return FloatingActionButton(
     backgroundColor: Theme.of(context).primaryColor,
-    child: Icon(Icons.add, color: Colors.white,),
+    child: Icon(
+      Icons.add,
+      color: Colors.white,
+    ),
     onPressed: () {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => CreateArea()));
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CreateArea(editMode: false),
+        ),
+      );
     },
   );
 }
